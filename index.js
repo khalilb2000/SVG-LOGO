748c0fcc8e206d184538ac319faef6d7acd15b97
khalilb2000 <khalilb2000@gmail.com>
2023-09-20 09:39:01 -0500

    Adding the array of questions as well as added in require/inquire functions and called in my libs.

---
+++ index.js
@@ -0,0 +1,57 @@
+const inquirer = require("inquirer");
+const {Circle,Triangle,Square} = require("./shapes")
+const sysytemfile = require("./node_modules")
+
+class SVG{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+        //returning ${this.shapeElement} and ${this.textElement}
+        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`
+    }
+
+    setShapeElement(shape){
+        this.shapeElement = shape.render()
+    }
+    setTextElement(text){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>`
+    }
+
+}
+
+const question = [
+
+    {
+type: "input",
+name: "text",
+message: "TEXT: Please enter up to (3) charaters:",
+
+    },
+
+    {
+        type:"input",
+        name:"text-color",
+        message: "TEXT-COLOR: Please enter a color keyword (OR hexadecimal number):"
+    },
+
+    {
+        type:"input",
+        name:"shape",
+        messgae: "SHAPE COLOR : Please enter a color keyword (OR hexadecimal number):"
+    },
+    {
+        type:"list",
+        name:"pixel-img"
+        
+    }
+
+
+
+
+
+
+
+
+]
