df6a2a4d8643d32a50aba25ee9c3a4c98fc69551
khalilb2000 <khalilb2000@gmail.com>
2023-09-20 11:02:44 -0500

    added in the color selector

--- shapes.test.js
+++ shapes.test.js
@@ -1,7 +1,23 @@
+const { Square, Circle, Triangle} = require("./shapes");
+
 //Circle
 describe('Circle', () => {
     const shape = new Circle();
     var color =('pink')
     shape.setColor(color);
-    expect
-})
+    expect(shape.render()).toEqual(`<circle cx =  "50%" cy = "50%" r = "100" height = "100" width = "100" fill="${color}"/>`)
+});
+
+describe('Square', () => {
+    const shape = new Square();
+    var color =('green')
+    shape.setColor(color);
+    expect(shape.render()).toEqual(`<rect  x = "50" height =  "100%" width =  "200%"  fill="${color}"/>`)
+});
+
+describe('Triangle', () => {
+    const shape = new Triangle();
+    var color =('white')
+    shape.setColor(color);
+    expect(shape.render()).toEqual(`<polygon height =  "100%" width =  "100%" points = "0,200,300,200,150,0" fill="${color}"/>`)
+});
